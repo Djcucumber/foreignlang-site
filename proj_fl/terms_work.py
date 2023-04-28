764cbb3a5770ed60a065f2097466c45296b26054
djcucumber <anytka.moxova2023@yandex.ru>
2023-04-28 23:50:38 +0300

    Начало изменения дизайна и верстки сайта

--- proj_fl/terms_work.py
+++ proj_fl/terms_work.py
@@ -28,7 +28,7 @@ def get_terms_stats():
     defin_len = []
     with open("./data/terms.csv", "r", encoding="utf-8") as f:
         for line in f.readlines()[1:]:
-            term, defin, added_by = line.split(";")
+            term, trans, defin, added_by = line.split(";")
             words = defin.split()
             defin_len.append(len(words))
             if "user" in added_by:
@@ -39,7 +39,7 @@ def get_terms_stats():
         "terms_all": db_terms + user_terms,
         "terms_own": db_terms,
         "terms_added": user_terms,
-        "words_avg": sum(defin_len)/len(defin_len),
+        "words_avg": round(sum(defin_len)/len(defin_len), 2),
         "words_max": max(defin_len),
         "words_min": min(defin_len)
     }
